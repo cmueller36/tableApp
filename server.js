aac68b903e78133a56b480cf5f97d3e7668541fa
Claire Stolp <clairemstolp@gmail.com>
2018-07-17 19:45:05 -0400

    added .gitignore

--- server.js
+++ server.js
@@ -8,7 +8,31 @@ var path = require("path");
 var app = express();
 var PORT = 8080;
 
+var tables = [
+  {
+    name: 'pikachu',
+    phone: '555-555-5555', 
+    email: 'pikapi@pokenet.com',
+    id: 0
+  }
+];
 
+var waitlist = [
+  {
+    name: 'MagicKarp',
+    phone: '111-111-1111',
+    email: 'magicKarpeDiem@pokenet.com',
+    id: 0
+  }
+]
+
+app.get("/api/tables", function (req, res) {
+  return res.json(tables);
+});
+
+app.get("/api/waitlist", function (req, res){
+  return res.json(waitlist);
+});
 
 
 //Start the server
